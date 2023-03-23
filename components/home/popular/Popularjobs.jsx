3749c41ef7fdb74eb0995ef815e73c0e5275eb60
Misha <misharexviashvili@yahoo.com>
2023-03-24 00:56:41 +0400

    fixed data fetching bug

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -11,9 +11,14 @@ import { useRouter } from "expo-router";
 import styles from "./popularjobs.style";
 import { COLORS, SIZES } from "../../../constants";
 import PopularJobCard from "../../common/cards/popular/PopularJobCard";
+import useFetch from "../../../hook/useFetch";
 const Popularjobs = () => {
-  const isLoading = false;
-  const error = false;
+  const router = useRouter();
+  const { data, isLoading, error } = useFetch("search", {
+    query: "React Developer",
+    num_pages: 1,
+  });
+  console.log(data);
   return (
     <View style={styles.container}>
       <View style={styles.header}>
